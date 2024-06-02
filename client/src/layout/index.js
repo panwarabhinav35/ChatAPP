d182fdf2605a72645d7649b4854e2dd6ec4e550a
Abhinav Panwar <panwarabhinav35@gmail.com>
2024-06-02 18:27:24 +0530

    new

---
+++ client/src/layout/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AuthLayouts = () => {
+  return (
+    <>
+      <div>
+        LOGO
+      </div>
+    </>
+  );
+};
+
+export default AuthLayouts;
---
+++ client/src/routes/index.js
@@ -0,0 +1,53 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import RegisterPage from "../pages/RegisterPage";
+import CheckEmailPage from "../pages/CheckEmailPage";
+import CheckPasswordPage from "../pages/CheckPasswordPage";
+import Home from "../pages/Home";
+import MessagePage from "../components/MessagePage";
+import AuthLayouts from "../layout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children: [
+      {
+        path: "register",
+        element: (
+          <AuthLayouts>
+            <RegisterPage></RegisterPage>
+          </AuthLayouts>
+        ),
+      },
+      {
+        path: "email",
+        element: (
+          <AuthLayouts>
+            <CheckEmailPage />
+          </AuthLayouts>
+        ),
+      },
+      {
+        path: "password",
+        element: (
+          <AuthLayouts>
+            <CheckPasswordPage />
+          </AuthLayouts>
+        ),
+      },
+      {
+        path: "",
+        element: <Home></Home>,
+        children: [
+          {
+            path: ":userId",
+            element: <MessagePage></MessagePage>,
+          },
+        ],
+      },
+    ],
+  },
+]);
+
+export default router;
